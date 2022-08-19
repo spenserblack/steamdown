e891e7c435007a74acac3b508049544a71262168
Spenser Black <spenserblack01@gmail.com>
2022-08-19 17:42:44 -0400

    Track coverage

--- packages/steamdown/jest.config.js
+++ packages/steamdown/jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
   moduleFileExtensions: ['js', 'ts'],
   testMatch: ['**/*.{test,spec}.{js,ts}'],
   transform: {
