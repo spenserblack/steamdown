60ff8164d2cce32047ad1694a774ba23613d1514
Spenser Black <spenserblack01@gmail.com>
2023-06-20 15:15:45 +0000

    Drop marked

--- packages/steamdown/src/index.ts
+++ packages/steamdown/src/index.ts
@@ -1,12 +1,6 @@
-import extensions from "./extensions";
-import { marked } from "marked";
-import renderer from "./renderer";
-
-marked.setOptions({ breaks: true });
-marked.use({ renderer, extensions });
 /**
  * Renders the given Markdown string to Steam's text format.
  */
 export default function parse(input: string): string {
-  return marked.parse(input).replace(/\n+$/, "");
+  throw new Error("Not implemented");
 }
