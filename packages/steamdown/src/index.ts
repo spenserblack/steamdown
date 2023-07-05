3ee3beada7c33f0c1417ca15402bdc0e61b8ac5f
Spenser Black <spenserblack01@gmail.com>
2023-07-05 17:58:24 +0000

    Use `input`
    Silences ESLint error until I get to this part.

--- packages/steamdown/src/index.ts
+++ packages/steamdown/src/index.ts
@@ -2,5 +2,5 @@
  * Renders the given Markdown string to Steam's text format.
  */
 export default function parse(input: string): string {
-  throw new Error("Not implemented");
+  throw new Error(`Not implemented, so ${input} cannot be parsed}`);
 }
