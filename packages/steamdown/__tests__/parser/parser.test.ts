732210b393853300832828dc0e531eb63c5da968
Spenser Black <spenserblack01@gmail.com>
2023-07-06 20:06:10 +0000

    Rename tests

--- packages/steamdown/__tests__/parser/parser.test.ts
+++ packages/steamdown/__tests__/parser/parser.test.ts
@@ -1,48 +1,44 @@
 import { describe, expect, test } from "@jest/globals";
 import Parser, { Heading } from "../../src/parser";
 
-describe("Parser", () => {
+describe.each([
+  ["# Heading 1", Heading],
+  ["## Heading 2", Heading],
+  ["### Heading 3", Heading],
+  ["#### Heading 4", Heading],
+  ["##### Heading 5", Heading],
+  ["###### Heading 6", Heading],
+  ["# Heading 1\n", Heading],
+  ["## Heading 2\n", Heading],
+  ["### Heading 3\n", Heading],
+  ["#### Heading 4\n", Heading],
+  ["##### Heading 5\n", Heading],
+  ["###### Heading 6\n", Heading],
+  ["# *Italic heading*", Heading],
+  ["# _Italic heading_", Heading],
+  ["# *Partially* italic heading", Heading],
+  ["# *Italic heading* with *italic text*", Heading],
+  ["# Heading with *italic tail*", Heading],
+  ["# **Bold heading**", Heading],
+  ["# Heading with **bold tail**", Heading],
+  ["# **Heading with *nested italics***", Heading],
+  ["# **Heading with *nested* italics**", Heading],
+])('Parser("%s")', (text, tokenType) => {
   describe(".next()", () => {
-    describe.each([
-      ["# Heading 1", Heading],
-      ["## Heading 2", Heading],
-      ["### Heading 3", Heading],
-      ["#### Heading 4", Heading],
-      ["##### Heading 5", Heading],
-      ["###### Heading 6", Heading],
-      ["# Heading 1\n", Heading],
-      ["## Heading 2\n", Heading],
-      ["### Heading 3\n", Heading],
-      ["#### Heading 4\n", Heading],
-      ["##### Heading 5\n", Heading],
-      ["###### Heading 6\n", Heading],
-      ["# *Italic heading*", Heading],
-      ["# _Italic heading_", Heading],
-      ["# *Partially* italic heading", Heading],
-      ["# *Italic heading* with *italic text*", Heading],
-      ["# Heading with *italic tail*", Heading],
-      ["# **Bold heading**", Heading],
-      ["# Heading with **bold tail**", Heading],
-      ["# **Heading with *nested italics***", Heading],
-      ["# **Heading with *nested* italics**", Heading],
-    ])("when given %p", (text, tokenType) => {
-      test("returns a token", () => {
-        const parser = new Parser(text);
-        const token = parser.next();
-        expect(token).toBeInstanceOf(tokenType);
-      });
+    const parser = new Parser(text);
+    const token = parser.next();
+    test(`Is instanceof ${tokenType}`, () => {
+      expect(token).toBeInstanceOf(tokenType);
+    });
 
-      test("inline tokens", () => {
-        const parser = new Parser(text);
-        const token = parser.next();
-        expect(token.tokens).toMatchSnapshot();
-      });
+    test(".tokens", () => {
+      expect(token.tokens).toMatchSnapshot();
+    });
 
-      test("rendered token", () => {
-        const parser = new Parser(text);
-        const token = parser.next();
-        expect(token.render()).toMatchSnapshot();
-      });
+    test(".render()", () => {
+      const parser = new Parser(text);
+      const token = parser.next();
+      expect(token.render()).toMatchSnapshot();
     });
   });
 });
