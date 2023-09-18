20052579ca4e3c39603c95f7a1325be29a0154f7
Spenser Black <spenserblack01@gmail.com>
2023-09-18 14:38:50 -0400

    TEMP: Switch out marked.js for remark

--- packages/steamdown/src/index.ts
+++ packages/steamdown/src/index.ts
@@ -1,12 +1,13 @@
-import extensions from "./extensions";
-import { marked } from "marked";
-import renderer from "./renderer";
+import remarkParse from 'remark-parse';
+import remarkGfm from "remark-gfm";
+import { unified } from "unified";
 
-marked.setOptions({ breaks: true });
-marked.use({ renderer, extensions });
+const processer = unified().use(remarkParse).use(remarkGfm);
+
 /**
  * Renders the given Markdown string to Steam's text format.
  */
-export default function parse(input: string): string {
-  return marked.parse(input).replace(/\n+$/, "");
+export default async function parse(input: string): Promise<string> {
+  const processed = await processer.process(input);
+  return String(processed);
 }
