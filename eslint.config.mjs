c7c491032dc314e81e475d4aca4a56fcda534b72
Spenser Black <spenserblack01@gmail.com>
2024-10-28 13:12:30 -0400

    Exempt built files from linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,7 @@ import tseslint from "typescript-eslint";
 
 export default [
   { files: ["**/*.{js,mjs,cjs,ts}"] },
-  { ignores: ["babel.config.js", "jest.config.js", "**/*.test.js"] },
+  { ignores: ["babel.config.js", "jest.config.js", "**/*.test.js", "**/dist/"] },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
