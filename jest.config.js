1d7f696226f884e51f9df588607d25981535d47a
Spenser Black <spenserblack01@gmail.com>
2024-11-06 13:40:59 -0500

    Fix testing
    Dependency was ESM, and needed to be transformed for testing.

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,6 @@ module.exports = {
   collectCoverage: true,
   coverageDirectory: "coverage",
   coverageProvider: "v8",
+  // NOTE Disables node_modules from being ignored during transformation.
+  transformIgnorePatterns: [],
 };
