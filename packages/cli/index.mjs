7e3026bb510e50a7c2b107e473d3df6d31c968cd
Spenser Black <spenserblack01@gmail.com>
2024-11-07 13:11:33 -0500

    Print AST with CLI

--- packages/cli/index.mjs
+++ packages/cli/index.mjs
@@ -1,2 +1,23 @@
 #!/usr/bin/env node
-console.log("Hello, world!");
+import { Command } from "commander";
+import { parse } from "@steamdown/core";
+import getStdin from "get-stdin";
+import fs from "node:fs/promises";
+
+const program = new Command();
+program
+  .name("steamdown")
+  .option("--ast", "Print the AST")
+  .argument('[file]', 'File to parse (STDIN if not specified)')
+  .action(async (file, options) => {
+    const content = file == null ? getStdin() : fs.readFile(file, "utf-8");
+    const parsed = parse(await content);
+
+    if (options.ast) {
+      console.log(JSON.stringify(parsed));
+      return;
+    }
+
+    throw new Error("Not implemented");
+  });
+program.parse();
