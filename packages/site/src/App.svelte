5125ff4ae2649d684b3a17820fc4613858039644
Spenser Black <spenserblack01@gmail.com>
2024-11-09 16:33:49 -0500

    Add some text describing prerelease

--- packages/site/src/App.svelte
+++ packages/site/src/App.svelte
@@ -4,6 +4,21 @@
 
 <main>
   <h1>Steamdown</h1>
+  <p>
+    Welcome to the beta test of the new version of Steamdown! Previously, it just used
+    an existing Markdown renderer, and used a bunch of settings overrides to hack it
+    into something that could render to Steam's markup. Now, it has its own parser and
+    renderer written specifically with Steam's markup in mind. There's some new syntax,
+    some changed syntax, and some removed syntax, with the goal of handling the
+    possibilities and limitations of Steam's markup. View the
+    <a href="https://github.com/spenserblack/steamdown">GitHub Repository</a>
+    for more information on the changes.
+  </p>
+  <p>
+    As this is a beta test, you may encounter some parsing or rendering issues. If you
+    do, please <a href="https://github.com/spenserblack/steamdown/issues/new">open an issue</a>
+    and report the problem.
+  </p>
   <Editor />
 </main>
 <footer>
