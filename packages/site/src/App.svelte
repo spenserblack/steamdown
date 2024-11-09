1d0dd429880c99a77e3a2d1927be9865acbb20e3
Spenser Black <spenserblack01@gmail.com>
2024-11-09 15:55:51 -0500

    Add "social" section

--- packages/site/src/App.svelte
+++ packages/site/src/App.svelte
@@ -6,3 +6,24 @@
   <h1>Steamdown</h1>
   <Editor />
 </main>
+<footer>
+  <h6>Social</h6>
+  <ul class="social">
+    <li><a href="https://github.com/spenserblack/steamdown">GitHub</a></li>
+  </ul>
+</footer>
+
+<style>
+  footer {
+    margin-top: 2rem;
+    text-align: right;
+  }
+  footer ul.social {
+    display: flex;
+    justify-content: right;
+    gap: 1rem;
+  }
+  footer ul.social li {
+    list-style: none;
+  }
+</style>
