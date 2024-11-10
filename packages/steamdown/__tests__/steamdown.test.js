a3fb3c95d910a9169574a02134bfe057da77bc4c
Spenser Black <spenserblack01@gmail.com>
2024-11-10 18:06:01 +0000

    Skip over `**` when closing italics

--- packages/steamdown/__tests__/steamdown.test.js
+++ packages/steamdown/__tests__/steamdown.test.js
@@ -12,6 +12,7 @@ describe("steamdown", () => {
   test.each([
     ["bold ending in italics (#233)", "**bold ending in *italics***", "[b]bold ending in [i]italics[/i][/b]"],
     ["bold beginning with italics (#233)", "***italics* and bold**", "[b][i]italics[/i] and bold[/b]"],
+    ["italics containing bold (#233)", "*italics **bold** italics*", "[i]italics [b]bold[/b] italics[/i]"],
   ])("%s", (_name, input, expected) => {
     const { tree, context } = parse(input);
     const rendered = render(tree, context);
--- packages/steamdown/src/parser/inline/italics.ts
+++ packages/steamdown/src/parser/inline/italics.ts
@@ -1,11 +1,35 @@
 import type * as nodes from "../../nodes";
 import type { Parser } from "../types";
+import { ParseError, UnreachableError } from "../errors";
 import { makeWrappedTextParser } from "./util.js";
+import { parse } from "./parse";
 
 /**
  * Parser for an italics node.
  */
-export const italics = makeWrappedTextParser<nodes.Italics>(
-  "*",
-  "italics",
-) satisfies Parser<nodes.Italics>;
+export const italics = {
+  hint: (text: string) => text.startsWith("*"),
+  parse: (text: string): [nodes.Italics, remainder: string] => {
+    text = text.slice(1);
+
+    // NOTE This might invalidate the text "*foo**", which could be considered as
+    //      italicized "foo" followed by "*". Supporting this malformed text is not a
+    //      priority, and the user can escape the trailing "*" to avoid this.
+    const close = /(?<!\\|\s|[^\\]\*)\*(?!\*)/.exec(text);
+    if (!close) {
+      throw new ParseError("italics must be closed");
+    }
+
+    const innerText = text.slice(0, close.index);
+    const remainder = text.slice(close.index + 1);
+
+    const nodes = parse(innerText);
+
+    const node: nodes.Italics = {
+      type: "italics",
+      nodes,
+    };
+
+    return [node, remainder];
+  },
+} satisfies Parser<nodes.Italics>;
