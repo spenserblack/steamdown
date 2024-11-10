cd05934bdd6ce4fba1b054c6ca797a13e87df83a
Spenser Black <spenserblack01@gmail.com>
2024-11-10 17:49:20 +0000

    Handle nested italics in bold text
    When parsing bold text, it will search for an opening italicization
    (`*`). If it's found, it then tries to find an ending italicization. If
    that is found, then that text is skipped when searching for the closing
    `**` for the bold text.

--- packages/steamdown/__tests__/steamdown.test.js
+++ packages/steamdown/__tests__/steamdown.test.js
@@ -8,4 +8,13 @@ describe("steamdown", () => {
     const rendered = render(tree, context);
     expect(rendered).toMatchSnapshot();
   });
+
+  test.each([
+    ["bold ending in italics (#233)", "**bold ending in *italics***", "[b]bold ending in [i]italics[/i][/b]"],
+    ["bold beginning with italics (#233)", "***italics* and bold**", "[b][i]italics[/i] and bold[/b]"],
+  ])("%s", (_name, input, expected) => {
+    const { tree, context } = parse(input);
+    const rendered = render(tree, context);
+    expect(rendered).toBe(expected);
+  });
 });
--- packages/steamdown/src/parser/inline/bold.ts
+++ packages/steamdown/src/parser/inline/bold.ts
@@ -1,11 +1,53 @@
 import type * as nodes from "../../nodes";
 import type { Parser } from "../types";
+import { ParseError, UnreachableError } from "../errors";
 import { makeWrappedTextParser } from "./util.js";
+import { parse } from "./parse";
 
 /**
  * Parser for a bold node.
  */
-export const bold = makeWrappedTextParser<nodes.Bold>(
-  "**",
-  "bold",
-) satisfies Parser<nodes.Bold>;
+export const bold = {
+  hint: (text: string) => text.startsWith("**"),
+  parse: (text: string): [nodes.Bold, remainder: string] => {
+    text = text.slice(2);
+    let searchIndex = 0;
+    const italicsStartRe = /(?<!\\)\*[^\s\*]/g;
+
+    // HACK Handle nested italics (**foo *bar* baz** and **foo *bar***). We do a simple
+    //      search for italics beginning and end markers, and skip over them.
+    while (true) {
+      const italicsStart = italicsStartRe.exec(text);
+      if (!italicsStart) {
+        break;
+      }
+      const italicsEndRe = /(?<!\\|\s)\*/g;
+      italicsEndRe.lastIndex = italicsStart.index + 1;
+      const italicsEnd = italicsEndRe.exec(text);
+      if (!italicsEnd) {
+        break;
+      }
+      searchIndex = italicsEnd.index + 1;
+      italicsStartRe.lastIndex = searchIndex;
+    }
+
+    const endRe = /(?<!\\|\s)\*\*/g;
+    endRe.lastIndex = searchIndex;
+    const endMatch = endRe.exec(text);
+    if (!endMatch) {
+      throw new ParseError("bold must be closed");
+    }
+
+    const innerText = text.slice(0, endMatch.index);
+    const remainder = text.slice(endMatch.index + 2);
+
+    const nodes = parse(innerText);
+
+    const node: nodes.Bold = {
+      type: "bold",
+      nodes,
+    };
+
+    return [node, remainder];
+  },
+} satisfies Parser<nodes.Bold>;
