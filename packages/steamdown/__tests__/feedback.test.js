4669ca367df3e288dda621fb966e2488580960de
Spenser Black <spenserblack01@gmail.com>
2025-03-29 14:44:19 -0400

    Add feedback tests (#292)
    This provides a way for the community to help by adding tests.

---
+++ packages/steamdown/__tests__/feedback.test.js
@@ -0,0 +1,25 @@
+const { parse, render } = require("../dist");
+
+/**
+ * Community-provided tests for expected behavior. Add your test cases here.
+ *
+ * @type [issueTitle: number | string, input: string, expected: string][]
+ */
+const feedback = [
+  // Example
+  ["Example", "*Hello, World!*", "[i]Hello, World![/i]"],
+];
+
+// -------------------------------------------------------------------------------------
+
+/** @type [title: string, input: string, expected: string][] */
+const tests = feedback.map(([title, ...test]) => [
+  typeof title === "number" ? `Issue #${title}` : title,
+  ...test,
+]);
+
+test.each(tests)("%s", (_title, input, expected) => {
+  const [tree, context] = parse(input);
+  const rendered = render(tree, context);
+  expect(rendered).toBe(expected);
+});
